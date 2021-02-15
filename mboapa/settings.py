742ad87448190eb1ee217701700308c4b1bec126
kwabena-aboah <obed30mintah@yahoo.com>
2021-02-15 17:18:54 +0000

    removed secret key

--- mboapa/settings.py
+++ mboapa/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'q&c4l-sznwzgpg3kq6lvno)lk=)uy%7@*t(dels*b1_m#nlfvs'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = True
